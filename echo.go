d5332cdff1852809674a2f0914dc95387ba4be9f
Ridho Febriansa <officialridhof@gmail.com>
2021-02-22 11:12:58 +0700

    memperbaiki typo pada e.Get dan menambahkan komentar pada endpoint function

--- echo.go
+++ echo.go
@@ -5,12 +5,13 @@ import (
   "github.com/labstack/echo"
 )
 
+// Hello menginformasikan bahwa API berjalan dengan baik
 func Hello(c echo.Context) error {
   return c.String(http.StatusOK, "Hello ALTA Store Customer")
 }
 
 func main() {
   e := echo.New()
-  e.Get("/hello", Hello)
+  e.GET("/hello", Hello)
   e.Logger.Fatal(e.Start(":8000"))
 }
