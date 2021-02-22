ad8c430eb8aaceee7e8f90436ab0110cd4ba4315
9tw <gerdotradawijaya@gmail.com>
2021-02-22 10:08:39 +0800

    Menambahkan Hello

--- echo.go
+++ echo.go
@@ -5,7 +5,12 @@ import (
   "github.com/labstack/echo"
 )
 
+func Hello(c echo.Context) error {
+  return c.String(http.StatusOK, "Hello ALTA Store Customer")
+}
+
 func main() {
   e := echo.New()
+  e.Get("/hello", Hello)
   e.Logger.Fatal(e.Start(":8000"))
 }
