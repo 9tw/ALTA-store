680aa0f98802f9eebfe5fa12a85411b0615705b5
Ridho Febriansa <21301894+ridhof@users.noreply.github.com>
2021-02-24 23:10:13 +0700

    Revert "API categories"

--- models/categories.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Categories struct {
-	Id						string `json: "id" form: "id"`
-	NamaKategori	string `json: "kategori" form: "kategori"`
-	KodeKategori	string `json: "kode" form: "kode"`
-}
