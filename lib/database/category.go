21a9439f486e059e7f3be09267940ecd7072b090
Ridho Febriansa <officialridhof@gmail.com>
2021-02-25 12:54:05 +0700

    fix: refactor categories attributes and add null check before insertion

--- lib/database/category.go
+++ lib/database/category.go
@@ -1,10 +1,12 @@
 package database
 
 import (
+	"errors"
 	"project/config"
 	"project/models"
 )
 
+// GetCategories returns all categories stored in database
 func GetCategories() (interface{}, error) {
 	var cats []models.Categories
 
@@ -14,6 +16,7 @@ func GetCategories() (interface{}, error) {
 	return cats, nil
 }
 
+// GetCategory returns category by given Category ID
 func GetCategory(id string) (interface{}, error) {
 	var cats []models.Categories
 
@@ -23,7 +26,12 @@ func GetCategory(id string) (interface{}, error) {
 	return cats, nil
 }
 
+// CreateCategories stores new category data to database
 func CreateCategories(cat *models.Categories) (interface{}, error) {
+
+	if cat.Kode == "" || cat.Nama == "" {
+		return nil, errors.New("Kode atau Nama Kategori tidak boleh kosong")
+	}
 
 	if err := config.DB.Save(cat).Error; err != nil {
 		return nil, err
--- models/categories.go
+++ models/categories.go
@@ -1,7 +1,8 @@
 package models
 
+// Categories struct contains Category object
 type Categories struct {
-	Id						string `json: "id" form: "id"`
-	NamaKategori	string `json: "kategori" form: "kategori"`
-	KodeKategori	string `json: "kode" form: "kode"`
+	ID   int    `json:"id" form:"id"`
+	Nama string `gorm:"size:24" json:"kategori" form:"kategori"`
+	Kode string `gorm:"size:10" json:"kode" form:"kode"`
 }
